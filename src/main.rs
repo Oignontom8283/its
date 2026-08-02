1abdf83814ae56c7a9d04e8c4a859be69dc1a0d9
Oignontom8283 <98918089+Oignontom8283@users.noreply.github.com>
2026-08-03 01:51:17 +0200

    feat(main): update main function to parse CLI arguments and display them (temp for dev)

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
+
+use clap::Parser;
+mod args;
+mod common;
+
 fn main() {
-    println!("Interactive Terminal's Selector (its) is currently under active development.");
-    println!("Please check the GitHub repository for updates: https://github.com/Oignontom8283/its");
+    let args = args::Cli::parse();
+
+    println!("Args : {:#?}", args);
 }
